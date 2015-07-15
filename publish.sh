3827d84da43fc2e5a00df2db967a44a884df0296
Sean McLaughlin <seanmcl@gmail.com>
2015-07-15 16:48:22 -0400

    publish script tweak

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ trap "kill 0" SIGINT SIGTERM
 bucket=www.seanmcl.com
 
 # Publish javascript and index file.
-if (( $# > 1 )); then
+if (( $# > 0 )); then
   npm run release
   aws s3 sync build s3://$bucket/build
   aws s3 cp public/prod.html s3://$bucket/index.html
